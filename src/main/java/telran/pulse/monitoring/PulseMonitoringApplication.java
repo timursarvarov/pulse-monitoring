54c2e01d410ce53d8a13d0b590a575c455434283
Yuragranovsky <yuragranovsky@gmail.com>
2022-04-03 15:58:48 +0300

    initial project structure

---
+++ src/main/java/telran/pulse/monitoring/PulseMonitoringApplication.java
@@ -0,0 +1,13 @@
+package telran.pulse.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/pulse/monitoring/PulseMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.pulse.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
